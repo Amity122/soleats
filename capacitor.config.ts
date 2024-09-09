fbc8caca8349e4b00fde7124979ebe19cd8079d9
Amity122 <amityelmwood@gmail.com>
2024-09-09 14:38:25 +0800

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'SolEats',
+  webDir: 'dist'
+};
+
+export default config;
